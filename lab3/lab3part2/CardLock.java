07b5d3697d64a6f14823f25db6e84f63613d8143
bryanvullo <bryanvullo04@gmail.com>
2022-10-17 21:50:20 +0100

    changing folder arrangement

---
+++ lab3/lab3part2/CardLock.java
@@ -0,0 +1,29 @@
+package lab3part2;
+
+public class CardLock {
+  private SmartCard lastCardSeen;
+  private Boolean studentAccess = false;
+
+  public void swipeCard(SmartCard card) {
+    this.lastCardSeen = card;
+  }
+
+  public SmartCard getLastCardSeen() {
+    return this.lastCardSeen;
+  }
+
+  public Boolean isUnlocked() {
+    // if the card type is staff or if the door allows student access
+    if (this.getLastCardSeen().isStaff() || this.studentAccess) {
+      return true;
+    }
+    // therefore if door is staff only and card type is student
+    else {
+      return false;
+    }
+  }
+
+  public void toggleStudentAccess() {
+    this.studentAccess = !this.studentAccess;
+  }
+}
---
+++ lab3/lab3part3/SmartCard.java
@@ -0,0 +1,24 @@
+package lab3part3;
+
+public class SmartCard {
+  private String owner;
+  private Boolean staffStatus = false;
+
+  public SmartCard(String name) {
+    this.owner = name;
+  }
+
+  public String getOwner() {
+    return this.owner;
+  }
+
+  public Boolean isStaff() {
+    return this.staffStatus;
+  }
+
+  public void setStaff(Boolean status) {
+    this.staffStatus = status;
+  }
+
+
+}
