31ee3a20e0d05cab27b161effa3cdd936b07b9d6
bryanvullo <bryanvullo04@gmail.com>
2022-10-17 21:43:02 +0100

    changing folder arrangement

--- Hello.java
+++
@@ -1,5 +0,0 @@
-public class Hello { // Class declaration
-  public static void main(String[] args) { //This is the 'main' method
-    System.out.println("Hello World!"); // This prints out "Hello World"
-  }
-}
