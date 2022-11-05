b7c2accea74b99914ee7c4eded609bc6e693d31a
bryanvullo <bryanvullo04@gmail.com>
2022-11-05 12:45:52 +0000

    complete

--- lab8/src/lab8part2/Parrot.java
+++ lab8/src/lab8part2/Parrot.java
@@ -1,4 +1,8 @@
+<<<<<<<< HEAD:lab8/src/lab8part2/Parrot.java
 package lab8part2;
+========
+package lab8part1;
+>>>>>>>> origin/master:lab8/src/lab8part1/Parrot.java
 
 public class Parrot extends Omnivore {
   public Parrot(String name, int age) {
