c68c6bbf85f13d189aa497e0d1d41a622b2afff6
bryanvullo <bryanvullo04@gmail.com>
2022-11-05 12:18:29 +0000

    lab 9 part 1 complete

---
+++ lab9/src/lab9part1/FlashCardReader.java
@@ -0,0 +1,38 @@
+package lab9part1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+public class FlashCardReader {
+  BufferedReader reader;
+
+  public FlashCardReader(String file) {
+    try {
+      reader = new BufferedReader(new FileReader(file));
+    }
+    catch (FileNotFoundException e) {
+      System.err.println(e + ": file not found");
+    }
+  }
+
+  public String getLine() {
+    try {
+      return reader.readLine();
+    }
+    catch (IOException e) {
+      System.err.println(e + ": unable to read next line");
+      return null;
+    }
+  }
+
+  public boolean fileIsReady() {
+    try {
+      return reader.ready();
+    }
+    catch (Exception e) {
+      System.err.println(e + ": file is not ready");
+      return false;
+    }
+  }
+}
---
+++ lab9/src/lab9part2/FlashCardReader.java
@@ -0,0 +1,39 @@
+package lab9part2;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FlashCardReader {
+  BufferedReader reader;
+
+  public FlashCardReader(String file) {
+    try {
+      reader = new BufferedReader(new FileReader(file));
+    }
+    catch (FileNotFoundException e) {
+      System.err.println(e + ": file not found");
+    }
+  }
+
+  public String getLine() {
+    try {
+      return reader.readLine();
+    }
+    catch (IOException e) {
+      System.err.println(e + ": unable to read next line");
+      return null;
+    }
+  }
+
+  public boolean fileIsReady() {
+    try {
+      return reader.ready();
+    }
+    catch (Exception e) {
+      System.err.println(e + ": file is not ready");
+      return false;
+    }
+  }
+}
