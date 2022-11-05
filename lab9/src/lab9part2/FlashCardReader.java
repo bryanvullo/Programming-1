f8f44684fc58bdae5bbf8dcae09f549b9ac49082
bryanvullo <bryanvullo04@gmail.com>
2022-11-05 15:01:35 +0000

    lab 9 part 2

--- lab9/src/lab9part2/FlashCardReader.java
+++ lab9/src/lab9part2/FlashCardReader.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FlashCardReader {
   BufferedReader reader;
+  ArrayList<FlashCard> flashCards = new ArrayList<FlashCard>();
 
   public FlashCardReader(String file) {
     try {
@@ -35,5 +37,21 @@ public class FlashCardReader {
       System.err.println(e + ": file is not ready");
       return false;
     }
+  }
+
+  public ArrayList<FlashCard> getFlashCards() {
+    boolean flag = true; //flag makes while loop run
+    while (flag) {
+      String line = getLine(); //gets next line of the text file
+      if (line == null) { //if it's the end of the file then exit while loop
+        flag = false;
+      }
+      else { //make a flash card using the line in the text file
+        String[] questionAnswer = line.split(":");
+        FlashCard myFlashCard = new FlashCard(questionAnswer[0], questionAnswer[1]);
+        flashCards.add(myFlashCard); //add flash card to the array list
+      }
+    }
+    return flashCards; //return the array list of flash cards
   }
 }
---
+++ lab9/src/lab9part2/Quiz.java
@@ -0,0 +1,33 @@
+package lab9part2;
+
+import java.util.ArrayList;
+
+public class Quiz {
+  FlashCardReader myReader;
+  ArrayList<FlashCard> myFlashCards = new ArrayList<FlashCard>();
+  Toolbox myToolBox = new Toolbox();
+
+  public Quiz(String fileName) {
+    myReader = new FlashCardReader(fileName);
+    myFlashCards = myReader.getFlashCards();
+    play();
+  }
+
+  public void play() {
+    for (FlashCard flashCard : myFlashCards) { //loops through the flash cards
+      System.out.println(flashCard.getQuestion()); //prints question
+      String answer = myToolBox.readStringFromCmd(); //takes answe
+      if (answer.equals(flashCard.getAnswer())) { //compares answers
+        System.out.println("right");
+      }
+      else {
+        System.out.println("wrong");
+        System.out.println("the correct answer is: " + flashCard.getAnswer());
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Quiz myQuiz = new Quiz("Questions.txt");
+  }
+}
