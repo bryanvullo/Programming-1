915f673d247caba45a87c360b0f96e5118552c4a
bryanvullo <bryanvullo04@gmail.com>
2022-11-14 19:02:52 +0000

    add coursework

---
+++ Coursework/people/conductors/Conductor.java
@@ -0,0 +1,62 @@
+package people.conductors;
+
+import java.util.ArrayList;
+import music.Composition;
+import music.MusicScore;
+import orchestra.Orchestra;
+import people.Person;
+import people.musicians.Musician;
+import utils.SoundSystem;
+
+public class Conductor extends Person {
+  private SoundSystem soundSystem;
+  public ArrayList<Musician> musicians = new ArrayList<Musician>();
+  public Orchestra orchestra = new Orchestra(); //public because EcsBandAid uses this
+  public Conductor(String name, SoundSystem soundSystem) {
+    super(name);
+    this.soundSystem = soundSystem;
+  }
+
+  public void registerMusician(Musician musician) {
+    musicians.add(musician);
+  }
+
+  public void deregisterMusician(Musician musician) {
+    musicians.remove(musician);
+  }
+
+  public void playComposition(Composition composition) {
+    MusicScore[] scores = composition.getScores();
+    for (MusicScore score : scores) { // loops through all the scores for each instrument
+      for (Musician musician: musicians) { //loops through each musician assigned to the conductor
+        if (score.getInstrumentID() == musician.getInstrumentID()) {
+          //if the score is for the instrument that the musician in playing
+          musician.readScore(score.getNotes(), score.isSoft());
+          int result = orchestra.sitDown(musician);
+          switch (result) {
+            case 0:
+              System.out.println("Musician " + musician.getName() + " has sat down");
+            case 1:
+              System.out.println("There's no space for " + musician.getName());
+            case 2:
+              System.out.println("Musician " + musician.getName() + " is already sat down");
+            default:
+              System.out.println("There's no space for " + musician.getName());
+          } //prints out the result of the sitDown method
+        }
+      }
+    } //all the musicians are now assigned their scores
+    // the composition is ready to be played
+    for (int i = 0; i < composition.getLength(); i++) {
+      try { //Thread.sleep needs to be wrapped in try/catch
+        orchestra.playNextNote();
+        Thread.sleep(composition.getNoteLength()); //how long the note is played for
+      }
+      catch (InterruptedException e) {
+        System.err.println(e);
+      }
+    }
+    //composition has ended so stop the music
+    soundSystem.init();
+  }
+}
---
+++ Coursework/people/musicians/Cellist.java
@@ -0,0 +1,61 @@
+package people.musicians;
+
+import people.Person;
+import utils.SoundSystem;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Cellist extends Person implements Musician {
+
+  private String instrument = "Cello";
+  private final int instrumentID = 43;
+  private final int SOFT = 50;
+  private final int LOUD = 100;
+  private int loudness;
+  private List<Integer> notes = new ArrayList<Integer>();
+  private Iterator<Integer> nextNote;
+  private int seat;
+  private SoundSystem soundSystem;
+
+  public Cellist(String name, SoundSystem soundSystem, int seat) {
+    super(name); //sets name
+    this.soundSystem = soundSystem; //sets sound system
+    setSeat(seat); //sets seat
+  }
+
+  public Cellist(String name, SoundSystem soundSystem) {
+    super(name); //sets name
+    this.soundSystem = soundSystem; //sets sound system
+  }
+
+  public void setSeat(int seat) {
+    this.seat = seat; //sets seat to given seat
+    soundSystem.setInstrument(this.seat, instrumentID);
+    //sets the sound system the instrument and seat
+  }
+
+  public void readScore(int[] notes, boolean soft) {
+    for (int note : notes) { //reads the notes into the list
+      this.notes.add(note);
+    }
+    nextNote = this.notes.iterator(); //sets the iterator
+    if (soft) { //sets the loudness
+      loudness = SOFT;
+    } else {
+      loudness = LOUD;
+    }
+  }
+
+  public void playNextNote() {
+    if (nextNote.hasNext()) { //checks if there is a note to be played
+      int note = nextNote.next(); //gets the next note
+      soundSystem.playNote(seat, note, loudness); //plays the note
+    }
+  }
+
+  public int getInstrumentID() {
+    return instrumentID;
+  }
+}
