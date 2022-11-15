af474bc381746cf6e58ccd31f80a7d062cb5b8df
bryanvullo <bryanvullo04@gmail.com>
2022-11-15 23:59:40 +0000

    new main method which takes configuration files and inputs new static methods to help the main method be more modular creates a new sound system and creates the musician arraylist started on creating the composition arraylist

--- Coursework/EcsBandAid.java
+++ Coursework/EcsBandAid.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 import music.Composition;
 import people.conductors.Conductor;
+import people.musicians.Cellist;
 import people.musicians.Musician;
+import people.musicians.Pianist;
+import people.musicians.Violinist;
 import utils.SoundSystem;
 
 public class EcsBandAid {
@@ -73,6 +80,84 @@ public class EcsBandAid {
     //I  remove them outside the previous loop to not encounter ConcurrentModificationException
     for (Musician musician : musiciansToRemove) {
       bandConductor.deregisterMusician(musician); //remove from the band
+    }
+  }
+
+
+  //static methods for run time execution
+  public static Scanner createReader(String filename) {
+    Scanner reader = null;
+    try {
+      File file = new File(filename);
+      reader = new Scanner(file);
+    } catch (FileNotFoundException e) {
+      System.err.println(e);
+      System.exit(0); //quits the program
+    }
+    return reader;
+  }
+
+  public static void main(String[] args) {
+    String musiciansFilename =  args[0];
+    String compositionsFilename =  args[1];
+    int years = Integer.parseInt(args[2]);
+
+    SoundSystem mySoundSystem = null;
+    Scanner musicianReader;
+    Scanner compositionReader;
+    Pattern name = Pattern.compile("Name", Pattern.CASE_INSENSITIVE);
+    Pattern tempo = Pattern.compile("Tempo", Pattern.CASE_INSENSITIVE);
+    Pattern length = Pattern.compile("Length", Pattern.CASE_INSENSITIVE);
+    ArrayList<Musician> myMusicians = new ArrayList<Musician>();
+    ArrayList<Composition> myCompositions = new ArrayList<Composition>();
+
+    //init the sound system
+    try {
+      mySoundSystem = new SoundSystem();
+    } catch (Exception e) {
+      System.err.println(e);
+      System.exit(0); //quits the program
+    }
+
+    //create the scanner objects
+    musicianReader = createReader(musiciansFilename);
+    compositionReader = createReader(compositionsFilename);
+
+    //create the musicians collection
+    while (musicianReader.hasNextLine()) {
+      String line = musicianReader.nextLine();
+      line.replace(")",""); //removes the trailing ) character at the end
+      String[] words = line.split("\\(" ); //splits the line into the two musician and instrument
+      Musician musician = null;
+      switch (words[1]) {
+        case "Piano":
+          musician = new Pianist(words[0], mySoundSystem);
+          break;
+        case "Cello":
+          musician = new Cellist(words[0], mySoundSystem);
+          break;
+        case "Violin":
+          musician = new Violinist(words[0], mySoundSystem);
+          break;
+        default:
+          System.err.println("we only accept piano, cello and violin musicians");
+          System.exit(0); //quits the program
+      }
+      myMusicians.add(musician);
+    }
+
+    //create the compositions collection
+    while (compositionReader.hasNextLine()) {
+      String line = compositionReader.nextLine();
+      if (name.matcher(line).find()) {
+        //name
+      } else if (tempo.matcher(line).find()) {
+        //tempo
+      } else if (length.matcher(line).find()) {
+        //length
+      } else {
+        //score
+      }
     }
   }
 }
